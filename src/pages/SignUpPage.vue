045cb9a9ea3c83e98e2062c240d0f1e3722faa8e
JannyDream <haruto.yamamoto.king@gmail.com>
2023-09-01 08:41:20 +0200

    fix: add confirm password input field

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -15,6 +15,10 @@
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
                     <input type="password" v-model="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
                 </div>
+                <div>
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                    <input type="password" v-model="password2" id="password2" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                </div>
                 <button type="submit" class="w-full text-white bg-purple-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Register</button>
                 <div class="text-sm text-center font-medium text-gray-500 dark:text-gray-300">
                     Already registered? <RouterLink to="login" class="text-purple-700 hover:underline dark:text-purple-500">Login here</RouterLink>
@@ -30,7 +34,8 @@ import { ref } from 'vue';
 
 const name = ref('');
 const email = ref('');
-const password = ref('')
+const password = ref('');
+const password2 = ref('');
 const registerUser = () => {
 
 }
