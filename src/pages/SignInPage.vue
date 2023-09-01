d4af06874ef158a5f4a1f49069cde227e7d66d3e
JannyDream <haruto.yamamoto.king@gmail.com>
2023-09-01 08:43:57 +0200

    fix: fix register link to signup

--- src/pages/SignInPage.vue
+++ src/pages/SignInPage.vue
@@ -13,7 +13,7 @@
                 </div>
                 <button type="submit" class="w-full text-white bg-purple-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Login to your account</button>
                 <div class="text-sm text-center font-medium text-gray-500 dark:text-gray-300">
-                    Not registered? <RouterLink to="register" class="text-purple-700 hover:underline dark:text-purple-500">Create account</RouterLink>
+                    Not registered? <RouterLink to="signup" class="text-purple-700 hover:underline dark:text-purple-500">Create account</RouterLink>
                 </div>
             </form>
         </div>
