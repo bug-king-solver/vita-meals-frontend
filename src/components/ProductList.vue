ca183eb70c849d3b9b141124c22fdd844a19b5d1
JannyDream <haruto.yamamoto.king@gmail.com>
2023-09-01 08:48:48 +0200

    feat: define productList component and add searchbox

---
+++ src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="product_list">
+        <div class="m-3 grid place-items-center">
+            <ProductSearch @handle-search="handleSubmit" />
+        </div>
+        
+        <div class="flex flex-wrap justify-center mt-3">
+        </div>
+    </div>
+    
+</template>
+
+<script setup lang="ts">
+const handleSubmit = () => {
+    
+}
+</script>
