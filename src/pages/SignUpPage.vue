2d798aa70e6e94376f895105a108c81143ef717e
JannyDream <haruto.yamamoto.king@gmail.com>
2023-09-01 08:44:21 +0200

    fix: fix login link to signin

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -21,7 +21,7 @@
                 </div>
                 <button type="submit" class="w-full text-white bg-purple-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Register</button>
                 <div class="text-sm text-center font-medium text-gray-500 dark:text-gray-300">
-                    Already registered? <RouterLink to="login" class="text-purple-700 hover:underline dark:text-purple-500">Login here</RouterLink>
+                    Already registered? <RouterLink to="signin" class="text-purple-700 hover:underline dark:text-purple-500">Login here</RouterLink>
                 </div>
             </form>
         </div>
