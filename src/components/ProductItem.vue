98a2b2e3b2f76328704db89b9716ea5f24fa97c4
JannyDream <haruto.yamamoto.king@gmail.com>
2023-09-03 15:10:30 +0200

    style: update productitem

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -7,26 +7,11 @@
       </RouterLink>  
       
       <div class="px-5 pb-5">
-        <div class="flex justify-between my-2">
-          <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            <RouterLink :to="{ name: 'product_details', params: { id: props.product.id } }">
-              {{ props.product.title }}
-            </RouterLink>
-          </h5>
-          <div class="flex">
-            <div @click.prevent="decrementCount" class="flex items-center px-2 cursor-pointer hover:bg-purple-400">
-              <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                  <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-              </svg>
-            </div>
-            <input class="mx-2 border rounded-md text-center w-10" v-model="count" type="number">
-            <div @click.prevent="incrementCount" class="flex items-center px-2 cursor-pointer hover:bg-purple-400">
-              <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                  <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-              </svg>
-            </div>
-          </div>
-        </div>
+        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <RouterLink :to="{ name: 'product_details', params: { id: props.product.id } }">
+            {{ props.product.title }}
+          </RouterLink>
+        </h5>
         <div class="flex items-center justify-between">
           <span class="text-3xl font-bold text-gray-900 dark:text-white"
             >${{ props.product.price }}</span
@@ -45,7 +30,6 @@
 <script setup lang="ts">
 import { RouterLink, useRouter } from 'vue-router';
 import { Product } from '../types';
-import { ref } from 'vue';
 import { useFlash } from '../common/useFlash';
 import { useAuthStore } from '../store';
 
@@ -58,23 +42,8 @@ const {flash} = useFlash();
 
 const authStore = useAuthStore();
 
-const count = ref(1)
-
-const decrementCount = () => {
-  count.value --;
-  if (count.value < 1) {
-    count.value = 1;
-  }
-}
-
-const incrementCount = () => {
-  count.value ++;
-  
-}
-
 const handAddToCart = () => {
   if (authStore.isAuthenticated) {
-    
   } else {
     flash({
       iconType: "warning",
