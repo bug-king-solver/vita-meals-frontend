8ce367a07c8c2f9a463e53aa8526a64fbd42ac40
JannyDream <haruto.yamamoto.king@gmail.com>
2023-09-06 20:47:46 +0200

    fix: update backend port

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
     // Base url of server side rest apis
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'http://localhost:8001/api/',
     headers: {
         'Accept': 'application/json'
     }
